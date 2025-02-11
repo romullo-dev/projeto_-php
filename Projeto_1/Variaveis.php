3b92bb71b75909f4951aeea46e7c6e174b3cc3e2
Rômulo França <157732149+romullo-dev@users.noreply.github.com>
2025-02-10 21:42:46 -0300

    commitq

---
+++ Projeto_1/Variaveis.php
@@ -0,0 +1,26 @@
+<?php
+    print"<h1>Teste</h1>";
+
+
+
+    //CONSTATE
+    define("NOME_SISTEMA","Sistema de gerenciador");
+
+    print NOME_SISTEMA;
+    
+    //VARIAVEL
+    $nome = "Maria";
+    echo $nome ;
+
+
+    echo "<BR>";
+
+    //DIFERENCIA ENTRE  '' E "" 
+    $nome = "CARLOS";
+    echo "<BR> olá , $nome" ;
+    echo '<BR> olá , $nome' ;
+
+    //CONCATENAR
+    echo '<BR> olá ,' .$nome ; //PONTO CONCATENA.
+
+?>
