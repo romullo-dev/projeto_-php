395c1b815f2338bd1d408ab8a68c9aac1c2b033b
Rômulo França <157732149+romullo-dev@users.noreply.github.com>
2025-02-10 21:23:21 -0300

    commit

--- Projeto_1/index.php
+++ Projeto_1/index.php
@@ -1,4 +1,13 @@
-<?php
-print "Ola mundo PHP";
-?>
-
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        print"Hello world";
+    ?>
+</body>
+</html>
---
+++ Projeto_1/pag.php
@@ -0,0 +1,14 @@
+<?php
+    print"<h1>Teste</h1>";
+
+
+
+    //CONSTATE
+    define("NOME_SISTEMA","Sistema de gerenciador");
+
+    print NOME_SISTEMA;
+    
+    //VARIAVEL
+    $nome = "Maria";
+    echo $nome ;
+?>
