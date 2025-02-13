a2b33e94bf9dcdfb3f605726a08968dc98fdd753
Rômulo França Dev <romulo.franca03@gmail.com>
2025-02-12 22:01:52 -0300

    commit

--- Aula_3/Atividade0.1/calc.php
+++ Aula_3/Atividade0.1/calc.php
@@ -1,7 +1,33 @@
 <?php 
+echo"Nome do cliente ".$nome_pessoa = $_POST["nome"] . "<br>";
 
-$a = $_POST["a"];
+$cachorro = $_POST["cachorro"];
+$salada = $_POST["salada"];
+$burg = $_POST["burg"];
+$batata = $_POST["batata"];
+$x = $_POST["x"];
+$refri = $_POST["refri"];
 
-print  $a;
+
+$Q_cachorro = $cachorro * 1.50;
+$Q_salada = $salada * 3;
+$Q_burg = $burg * 2;
+$Q_batata = $batata * 2.50;
+$Q_x = $x * 4.50;
+$Q_refri = $refri * 2;
+
+$soma = $Q_cachorro + $Q_salada + $Q_burg + $Q_batata + $Q_x + $Q_refri;
+
+echo "Consumo do cliente " .$soma . "<br><br>";
+echo 'Cachorro-Quente '.$cachorro."<br>";
+echo 'X-Salada ' .$salada."<br>";
+echo 'X-Burger '.$burg."<br>";
+echo 'Batata Frita '.$batata."<br>";
+echo 'X-Tudo '.$x."<br>";
+echo 'Refrigerante Lata ' .$refri."<br>";
+
+
+
+
 
 ?>
--- Aula_3/Atividade0.1/index.php
+++ Aula_3/Atividade0.1/index.php
@@ -7,15 +7,62 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+    <div class="container mt-4">
         <h2 class="text-success">Net Lanches - Fast Food</h2>
-
         <form action="calc.php" method="post">
-        <input type="text" name="a" >
-        <button type="reset" class="btn btn-secondary">Limpar Comanda</button>
-        <input type="submit" value="Enviar" >
-        
+            <div class="mb-3">
+                <label for="cliente" class="form-label fw-bold text-primary">Nome do Cliente:</label>
+                <input type="text" class="form-control" id="cliente" placeholder="Digite seu nome" name="nome" >
+            </div>
+
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Lanches</th>
+                        <th>Valor</th>
+                        <th>Quantidade</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><a href="#" class="text-decoration-none text-primary">Cachorro-Quente</a></td>
+                        <td>R$ 1.50</td>
+                        <td><input type="number" class="form-control" min="0" name="cachorro" ></td>
+                    </tr>
+                    <tr>
+                        <td><a href="#" class="text-decoration-none text-primary">X-Salada</a></td>
+                        <td>R$ 3.00</td>
+                        <td><input type="number" class="form-control" min="0" name="salada"></td>
+                    </tr>
+                    <tr>
+                        <td><a href="#" class="text-decoration-none text-primary">X-Burger</a></td>
+                        <td>R$ 2.00</td>
+                        <td><input type="number" class="form-control" min="0" name="burg"></td>
+                    </tr>
+                    <tr>
+                        <td><a href="#" class="text-decoration-none text-primary">Batata Frita</a></td>
+                        <td>R$ 2.50</td>
+                        <td><input type="number" class="form-control" min="0" name="batata"></td>
+                    </tr>
+                    <tr>
+                        <td><a href="#" class="text-decoration-none text-primary">X-Tudo</a></td>
+                        <td>R$ 4.50</td>
+                        <td><input type="number" class="form-control" min="0" name="x"></td>
+                    </tr>
+                    <tr>
+                        <td><a href="#" class="text-decoration-none text-primary">Refrigerante Lata</a></td>
+                        <td>R$ 2.00</td>
+                        <td><input type="number" class="form-control" min="0" name="refri"></td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <div class="d-flex justify-content-between">
+                <button type="reset" class="btn btn-secondary">Limpar Comanda</button>
+                <button type="submit" class="btn btn-success">Enviar Pedido</button>
+            </div>
         </form>
-                
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
