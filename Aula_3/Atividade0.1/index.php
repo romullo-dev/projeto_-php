d250e97820bfda9d10424e4339fcc3ed59939dfe
Rômulo França Dev <romulo.franca03@gmail.com>
2025-02-12 21:28:27 -0300

    commit

---
+++ Aula_3/Atividade0.1/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Net Lanches - Fast Food</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+        <h2 class="text-success">Net Lanches - Fast Food</h2>
+
+        <form action="calc.php" method="post">
+        <input type="text" name="a" >
+        <button type="reset" class="btn btn-secondary">Limpar Comanda</button>
+        <input type="submit" value="Enviar" >
+        
+        </form>
+                
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
