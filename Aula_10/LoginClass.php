53df57b2b12057ac4214aa1611d08fa90f9b28a5
Rômulo França <157732149+romullo-dev@users.noreply.github.com>
2025-02-21 20:45:27 -0300

    commit

---
+++ Aula_10/LoginClass.php
@@ -0,0 +1,48 @@
+<?php
+
+// Classe Login
+class LoginClass
+{
+    // Atributos
+    private $login = null;
+    private $senha = null;
+
+    // Getters e Setters
+    public function setLogin($login)
+    {
+        $this->login = $login;
+    }
+
+    public function getLogin()
+    {
+        return $this->login;
+    }
+
+    public function setSenha($senha)
+    {
+        $this->senha = $senha;  
+    }
+
+    public function getSenha()
+    {
+        return $this->senha;
+    }
+
+    // Método de Login
+    public function efetuarLogin($login, $senha)
+    {
+        $this->setLogin($login);
+        $this->setSenha($senha);
+
+        // Verificação do login e senha
+        if ($this->getLogin() == 'maria' && $this->getSenha() == '123456') {
+            $_SESSION['login'] = $login;
+            header('Location: principal.php');
+            exit(); 
+        } else {
+            // Se o login 
+            echo "Login ou senha incorretos!";
+        }
+    }
+}
+?>
---
+++ Aula_10/login.php
@@ -0,0 +1,11 @@
+<?php
+
+//iniciaer sessao
+session_start();
+//incluindo a classe
+include_once("LoginClass.php");
+
+//criar objeto/instancia
+$acessar = new LoginClass();
+//invocar o metodo
+$acessar ->efetuarLogin($_POST['login'],$_POST['senha']);
